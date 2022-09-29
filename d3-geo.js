3cbc5e2cb444561f4286874cd33760eb89570174
Sarah Gebauer <xgebi@users.noreply.github.com>
2022-09-29 21:52:37 +0200

    Almost done

---
+++ d3-geo.js
@@ -0,0 +1,11 @@
+/*
+ * Skypack CDN - d3-geo@3.0.1
+ * Remove "min" from url for additional help & instructions.
+ */
+
+export * from '/-/d3-geo@v3.0.1-kwyelOm8gApBxT2oVVB9/dist=es2020,mode=imports,min/optimized/d3-geo.js';
+export {default} from '/-/d3-geo@v3.0.1-kwyelOm8gApBxT2oVVB9/dist=es2020,mode=imports,min/optimized/d3-geo.js';
+
+// Perf Optimization: Preload Deep Imports
+import "/-/d3-array@v3.1.1-Ibshj34oOmCw8da1RLSW/dist=es2020,mode=imports,min/optimized/d3-array.js";
+import "/-/internmap@v2.0.3-GWZlRrRMFcDlELwTQEZq/dist=es2020,mode=imports,min/optimized/internmap.js";
---
+++ legal stuff.md
@@ -0,0 +1,11 @@
+# Fonts
+Noto Sans is distributed under OFL
+
+# States & districts json
+MIT license
+https://github.com/deldersveld/topojson
+
+# Images
+Sourced from Unsplash
+https://unsplash.com/photos/UXQJ5PaRvgM
+https://unsplash.com/photos/H0Jp8pX-0zw
